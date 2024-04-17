9cf6825ef0c8cf97e638f6657dfc9e1c66b430ed
Islam <isllamessam@gmail.com>
2024-04-17 15:03:35 +0200

    di

---
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.loc.newsapp.di
+
+import android.app.Application
+import com.loc.newsapp.data.manger.LocalUserMangerImpl
+import com.loc.newsapp.domain.manger.LocalUserManger
+import com.loc.newsapp.domain.usecases.AppEntryUseCases
+import com.loc.newsapp.domain.usecases.ReadAppEntry
+import com.loc.newsapp.domain.usecases.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideLocalUserManger(
+        application: Application
+    ): LocalUserManger = LocalUserMangerImpl(application)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(
+        localUserManger: LocalUserManger
+    ) = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+}
